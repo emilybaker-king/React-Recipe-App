acf45549e3be64c8ff468e7ced109bae4521e15f
Emily Baker-King <ebakerking@gmail.com>
2019-11-13 12:47:22 -0600

    added Header component and set up UI for it

--- src/RecipeSearcher/RecipeSearcher.js
+++ src/RecipeSearcher/RecipeSearcher.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import RecipeList from '../RecipeList/RecipeList';
+import Header from '../Header/Header';
 
 class RecipeSearcher extends Component {
 
@@ -76,6 +77,7 @@ class RecipeSearcher extends Component {
 
         return (
             <div>
+                <Header />
                 <RecipeList recipes={this.state.recipes} />
             </div>
         );
